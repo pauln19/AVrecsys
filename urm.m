c1fee19eb96edd7aee57b29a8e0ede242846cc14
pauln19 <khanh.tran@mail.polimi.it>
2016-11-10 11:28:11 +0100

    partial rec cf plus urm

---
+++ urm.m
@@ -0,0 +1,22 @@
+v = [1:size(usercareerlevel,1)].';
+userMap = containers.Map(sort(usercareerlevel(:,1)),v);
+
+v = [1:size(M,1)].';
+itemMap = containers.Map(M(:,1),v);
+
+sparseI = zeros(1,size(interactions,1));
+sparseJ = zeros(1,size(interactions,1));
+sparseV = zeros(1,size(interactions,1));
+
+for i = 1:size(interactions,1)
+    
+    user = values(userMap,num2cell(interactions(i,1)));
+    item = values(itemMap,num2cell(interactions(i,2)));
+    
+    sparseI(1,i) = cell2mat(user);
+    sparseJ(1,i) = cell2mat(item);
+    sparseV(1,i) = interactions(i,3);
+    
+end
+
+URM = sparse(sparseI, sparseJ, sparseV);
